6db3a332429777a816ea10e0c305a839eaf274f8
lauraturner <37046209+lauraturner@users.noreply.github.com>
2019-11-03 20:03:34 -0500

    Add module to get code from sample text file

--- code_builder.py
+++ code_builder.py
@@ -1,16 +1,42 @@
+from collections import OrderedDict
 import collections
+def write_code_file(code):
+    print(code)
+    with open('test.txt', 'a') as the_file:
+        for ltr in code.keys():
+            ltr_code = code[ltr][ : : -1]
+            the_file.write(ltr + '\t' + "".join(ltr_code) + '\n')
 
-def get_code(txt_file, asciiDict):
+def add_code(ltr1, ltr2, ltr_code):
+    for ltr in ltr1[0]:
+        ltr_code[ltr].append('1')
+    for ltr in ltr2[0]:
+        ltr_code[ltr].append('0')
+    return ltr_code
+
+def get_code(ltr_code, ltr_groups):
+    if len(ltr_groups) == 1:
+        return ltr_code
+    ltr1 = ltr_groups.popitem()
+    ltr2 = ltr_groups.popitem()
+    ltr_code = add_code(ltr1, ltr2, ltr_code)  
+    ltr_groups[ltr1[0] + ltr2[0]] = ltr1[1] + ltr2[1]
+    ltr_groups = OrderedDict(sorted(ltr_groups.items(), key=lambda x: x[1], reverse=True))
+    return get_code(ltr_code, ltr_groups)
+
+def get_frequency(txt_file, asciiDict):
     for line in txt_file:
         for char in line:
             asciiDict[char] += 1
     asciiDict = {x:y for x,y in asciiDict.items() if y!=0}
-    sorted_ascii = sorted(asciiDict.items(), key=lambda kv: kv[1])
+    sorted_ascii = sorted(asciiDict.items(), key=lambda kv: kv[1], reverse=True)
     asciiDict = collections.OrderedDict(sorted_ascii)
     return asciiDict
 
 asciiDict = {chr(i): 0 for i in range(32, 127)}
 asciiDict['\n'] = 0
 txt_file = open("words1ASCII.txt", "r")
-code = get_code(txt_file, asciiDict)
-print(code)
+ltr_frequency = get_frequency(txt_file, asciiDict)
+ltr_code = {x: [] for x in ltr_frequency.keys()}
+code = get_code(ltr_code, ltr_frequency)
+write_code_file(code)
